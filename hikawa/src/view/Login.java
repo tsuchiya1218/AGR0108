10612ba614aaa0475a31f1e5e4338b0d21395d25
20jy0111 <20jy0111@PC14.jynet.jec.ac.jp>
2022-01-12 15:02:15 +0900

    Jrabel追加

--- hikawa/src/view/Login.java
+++ hikawa/src/view/Login.java
@@ -1,11 +1,39 @@
 package view;
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Container;
 
-public class Login {
-	public static void main(String args[]){
-	    JFrame frame = new JFrame();
-	    frame.setTitle("MyTitle");
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+class Login extends JFrame {
+	public static void main(String args[]) {
+	    Login frame = new Login("Login");
 	    frame.setVisible(true);
-	  }
+	}
+	
+	Login(String title){
+	    setTitle(title);
+	    setSize(600, 400);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    JPanel p = new JPanel();
+	    
+	    JLabel label1 = new JLabel("従業員番号：");
+	    JLabel label2 = new JLabel("パスワード");
+	    
+	    JTextField EPYcodetxt = new JTextField();
+	    JTextField Passtxt = new JTextField();
+	    
+	    p.add(label1);
+	    p.add(EPYcodetxt);
+	    p.add(label2);
+	    p.add(Passtxt);
+	    
+	    Container contentPane = getContentPane();
+	    contentPane.add(p, BorderLayout.CENTER);
+	}
+	
 	
 }
