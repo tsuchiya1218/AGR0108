d89d4e119e55dcd737ca9ea9143c4f9f75d0ee5c
shota <shota@DESKTOP-1LGO33B>
2022-01-17 11:42:07 +0900

    データベース接続時のURL,user,passを追加しました。接続時とクローズの処理をtry・catchで囲みました。

--- hikawa/src/dao/DBAccess.java
+++ hikawa/src/dao/DBAccess.java
@@ -4,20 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBAccess {
-	//データベース接続
-			String url = "";
-			String user = "";
-			String pass = "";
-			DriverManager out;
-	
 	private Connection con =null;
 
 	public Connection createConnection() throws Exception {
-			con = DriverManager.getConnection(url,user,pass);
-			return con;
+		try {
+			//データベース接続
+			con = DriverManager.getConnection("jdbc:mysql://10.64.144.5:3306/20jy0125","20jy0125","20jy0125");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+			
 	}
 	public void closeConnection(Connection con) throws Exception{
-		//データベースクローズ
-		con = DriverManager.getConnection(url,user,pass);
+		if(con != null) {
+			try{
+				//データベースクローズ
+				con.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
+
