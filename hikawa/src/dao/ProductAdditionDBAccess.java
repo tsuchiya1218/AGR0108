12ffdd8d8481cec6b9b982092a6c3efde0ba9021
20jy0125 <20jy0125@jec.ac.jp>
2022-01-27 12:08:09 +0900

    更新しました

--- hikawa/src/dao/ProductAdditionDBAccess.java
+++ hikawa/src/dao/ProductAdditionDBAccess.java
@@ -10,26 +10,37 @@ import java.sql.ResultSet;
 
 public class ProductAdditionDBAccess {
 	String pName,mName,cID;
-	String makerID = getMakerIDbyName();
+	String makerID = getMakerIDbyName(mName);//makerIDを取得
 	int price,foodLCode;
+	String pCode = makeProductCode();//商品コード生成
+	
 	ProductAdditionDBAccess(String pName, String mName, String cID, int price, int foodLCode){
+		//引数(商品名、メーカー名、カテゴリID、価格、食品期限コード)
 		this.pName = pName;
 		this.mName = mName;
 		this.cID = cID;
 		this.price = price;
 		this.foodLCode = foodLCode;
 	}
+	
+	
+	//商品を新規作成する
 	public void productAddition() {
-		//引数(商品名、メーカー名、カテゴリID、価格、食品期限コード)
 		Connection con = null;
 		DBAccess db = new DBAccess();
 		ResultSet rs = null;
 		PreparedStatement ps = null;
+		if(makerID == null) {
+			makerID = makeMakerID();
+			makeMaker(makerID,mName);
+		}
 		try {
-			
 			con = db.createConnection();
+			String sql = 
+					"INSERT INTO product(ProductCode,ProductName,Price,FoodLimitCode,MakerID,CategoryID) VALUES("
+					+")";
 			//ProductCode,ProductName,Price,FoodLimitCode,MakerID,CategoryIDを商品表に追加
-			ps = con.prepareStatement("INSERT ");
+			ps = con.prepareStatement(sql);
 			rs = ps.executeQuery();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -38,8 +49,8 @@ public class ProductAdditionDBAccess {
 		
 	}
 	//メーカー名からメーカーＩＤを取得する(存在しなければNULLを返す)
-	public String getMakerIDbyName() throws Exception {
-		String makerID = null;
+	public String getMakerIDbyName(String mName) {
+		String mID = null;
 		Connection con = null;
 		DBAccess db = new DBAccess();
 		ResultSet rs = null;
@@ -51,10 +62,54 @@ public class ProductAdditionDBAccess {
 			rs = ps.executeQuery();
 			while(rs.next()) {
 				if(rs.getString("makerID") != null) {
-					makerID = rs.getString("makerID");
+					mID = rs.getString("makerID");
 				}
 			}
 		}catch(Exception e){
+			e.printStackTrace();
+			mID = null;
+		}finally {
+			if(rs!= null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(ps!= null) {
+				try {
+					ps.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				db.closeConnection(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return mID;
+	}
+	
+	
+	//makerIDを作成
+	public String makeMakerID() {
+		String mID = null;
+		int cnt = 0;
+		Connection con = null;
+		DBAccess db = new DBAccess();
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		
+		try {
+			con = db.createConnection();
+			ps = con.prepareStatement("SELECT COUNT(*) cnt FROM maker");
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				cnt = rs.getInt("cnt");
+			}
+		}catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			if(rs!= null) {
@@ -71,8 +126,92 @@ public class ProductAdditionDBAccess {
 					e.printStackTrace();
 				}
 			}
-			db.closeConnection(con);
+			try {
+				db.closeConnection(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		return makerID;
+		cnt = cnt + 1;
+		mID  = String.valueOf(cnt);
+		return mID;
+	}
+	
+	//新しいメーカーを作成する
+	public void makeMaker(String makerID, String makerName) {
+		Connection con = null;
+		DBAccess db = new DBAccess();
+		PreparedStatement ps = null;
+		try {
+			con = db.createConnection();
+			ps = con.prepareStatement("INSERT INTO maker VALUE(?,?)");
+			ps.setString(1, makerID);
+			ps.setString(2, makerName);
+			//insertを実行
+			ps.executeUpdate();
+			//コミット
+			con.commit();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(ps!= null) {
+				try {
+					ps.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				db.closeConnection(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	/*
+	 * 商品コードを作成するクラス
+	 * カテゴリID_そのカテゴリの商品数+1
+	 */
+	public String makeProductCode() {
+		String ProductCode = null;
+		int cnt = 0;
+		Connection con = null;
+		DBAccess db = new DBAccess();
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		try {
+			con = db.createConnection();
+			ps = con.prepareStatement("SELECT COUNT(*) cnt FROM product WHERE CategoryID = ?");
+			ps.setString(1, cID);
+			rs = ps.executeQuery();
+			cnt = rs.getInt("cnt") + 1;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!= null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(ps!= null) {
+				try {
+					ps.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				db.closeConnection(con);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		ProductCode = cID + "_" + cnt;
+		return ProductCode;
 	}
 }
