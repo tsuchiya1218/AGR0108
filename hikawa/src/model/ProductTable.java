e5ca448c714e61dc48baf461a1ad6b19b6ba1dde
20jy0125 <20jy0125@jec.ac.jp>
2022-02-03 12:34:38 +0900

    priceが仕入れ価格とわかりやすくpurPriceに変更しました（DB同様）

--- hikawa/src/model/ProductTable.java
+++ hikawa/src/model/ProductTable.java
@@ -3,16 +3,16 @@ package model;
 public class ProductTable {
 	private int ProductTableID; //商品表ID
 	private int Stock; //在庫量
-	private int Price; //仕入れ価格
+	private int purPrice; //仕入れ価格
 	private String OrderCode; //発注コード
 
 	public ProductTable() {
 	}
 
-	public ProductTable(int ProductTableID, int Stock, int Price, String OrderCode) {
+	public ProductTable(int ProductTableID, int Stock, int purPrice, String OrderCode) {
 		this.ProductTableID = ProductTableID;
 		this.Stock = Stock;
-		this.Price = Price;
+		this.purPrice = purPrice;
 		this.OrderCode = OrderCode;
 	}
 
@@ -32,12 +32,12 @@ public class ProductTable {
 		return Stock;
 	}
 
-	public void setPrice(int Price) {
-		this.Price = Price;
+	public void setPrice(int purPrice) {
+		this.purPrice = purPrice;
 	}
 
 	public int getPrice() {
-		return Price;
+		return purPrice;
 	}
 	public void setOrderCode(String OrderCode) {
 		this.OrderCode = OrderCode;
