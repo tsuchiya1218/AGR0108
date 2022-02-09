fcf2681b994119754292167bc8008b353132079f
20jy0111 <20jy0111@PC46.jynet.jec.ac.jp>
2022-02-09 17:46:04 +0900

    数値判定追加

--- hikawa/src/view/Login.java
+++ hikawa/src/view/Login.java
@@ -17,7 +17,7 @@ import javax.swing.border.EmptyBorder;
 import control.HikawaController;
 import dao.LoginDBAccess;
 
-public class Login extends JFrame implements ActionListener{
+public class Login extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private TextField StaffIDText;
@@ -33,94 +33,107 @@ public class Login extends JFrame implements ActionListener{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(null);
-		
+
 		JLabel label = new JLabel("従業員番号: ");
 		label.setBounds(98, 97, 68, 16);
 		panel_1.add(label);
-		
+
 		StaffIDText = new TextField();
 		StaffIDText.setBounds(176, 94, 145, 23);
 		StaffIDText.setColumns(15);
 		panel_1.add(StaffIDText);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("パスワード: ");
 		lblNewLabel_1.setBounds(98, 129, 74, 16);
 		panel_1.add(lblNewLabel_1);
-		
+
 		PassField = new JPasswordField();
 		//TextField textField_1 = new TextField();
 		PassField.setBounds(176, 128, 145, 23);
 		panel_1.add(PassField);
-		
+
 		JButton btnLogin = new JButton("ログイン");
 		btnLogin.setBounds(102, 191, 92, 26);
 		btnLogin.setActionCommand("Login");
 		btnLogin.addActionListener(this);
 		panel_1.add(btnLogin);
-		
+
 		JButton btnreset = new JButton("リセット");
 		btnreset.setBounds(226, 191, 92, 26);
 		btnreset.setActionCommand("reset");
 		btnreset.addActionListener(this);
 		panel_1.add(btnreset);
-		
+
 		JLabel lblNewLabel = new JLabel("ログイン画面");
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblNewLabel.setBounds(147, 41, 136, 23);
 		panel_1.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("hikawaシステム");
 		lblNewLabel_2.setBounds(12, 12, 92, 16);
 		panel_1.add(lblNewLabel_2);
-		
-		
-	}
-	
-	
-	public void actionPerformed(ActionEvent e){
-		JFrame frame = new JFrame();
-	    String cmd = e.getActionCommand();
-	    
-	    //結果
-	    boolean judge ;
-	    
-	    //ログインボタンが押された時の処理
-	    if (cmd.equals("Login")){
-	    	LoginDBAccess lb = new LoginDBAccess();
-	    	//従業員番号取得
-	    	Integer StaffID = Integer.parseInt(StaffIDText.getText());
-	    	//パスワード取得
-	    	char[] passwordchar = PassField.getPassword();
-	    	String password = new String(passwordchar);
-	    	
-	    	//LoginDBA引渡し
-	    	try {
-				judge = lb.login(StaffID, password);
-				//判定true画面遷移falseエラー表示
-				if(judge) {
-					setVisible(false);
-			    	HikawaController.TopDisplay();
-				}else {
-					JOptionPane.showMessageDialog(frame, "従業員番号かパスワードが間違っています");
-				}
-				
-				
-			} catch (Exception e1) {
-				// TODO 自動生成された catch ブロック
-				e1.printStackTrace();
-			}
-	    	
-	    }
-	    
-	    //リセットボタンが押された時の処理
-	    if (cmd.equals("reset")){
-	    	StaffIDText.setText("");
-	    	PassField.setText("");
-	    }
-	 }
-}
 
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JFrame frame = new JFrame();
+		String cmd = e.getActionCommand();
+
+		//結果
+		boolean judge;
+
+		//setVisible(false);
+		//HikawaController.TopDisplay();
+
+		//ログインボタンが押された時の処理
+		if (cmd.equals("Login")) {
+			LoginDBAccess lb = new LoginDBAccess();
+
+			//従業員番号取得
+			//Integer StaffID = Integer.parseInt(StaffIDText.getText());
+			String StaffID = StaffIDText.getText();
+
+			//パスワード取得
+			char[] passwordchar = PassField.getPassword();
+			String password = new String(passwordchar);
+
+			//数字判定
+			if (!StaffID.matches("[+-]?\\d*(\\.\\d+)?")) {
+				//数値じゃなかったら入力欄初期化
+				StaffIDText.setText("");
+				PassField.setText("");
+				JOptionPane.showMessageDialog(frame, "従業員番号には数値のみ入力可能です。");
+
+			} else {
+
+				//LoginDBA引渡し
+				try {
+
+					judge = lb.login(Integer.parseInt(StaffID), password);
+					//判定true画面遷移falseエラー表示
+					if (judge) {
+						setVisible(false);
+						HikawaController.TopDisplay();
+					} else {
+						JOptionPane.showMessageDialog(frame, "従業員番号かパスワードが間違っています");
+					}
+
+				} catch (Exception e1) {
+					// TODO 自動生成された catch ブロック
+					e1.printStackTrace();
+				}
+			}
+
+		}
+
+		//リセットボタンが押された時の処理
+		if (cmd.equals("reset")) {
+			StaffIDText.setText("");
+			PassField.setText("");
+		}
+	}
+}
