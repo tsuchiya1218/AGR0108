e155498882bef51412af37db81b12f1b80696eb1
20jy0125 <20jy0125@jec.ac.jp>
2022-02-16 13:53:50 +0900

    addOrderHistory()が完成しました。

--- hikawa/src/dao/OrderHistoryDBAccess.java
+++ hikawa/src/dao/OrderHistoryDBAccess.java
@@ -2,7 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
+
+import action.CreateTableData;
 
 //発注履歴のデータベースアクセス
 public class OrderHistoryDBAccess {
@@ -15,15 +18,82 @@ public class OrderHistoryDBAccess {
 	//発注履歴を追加する
 	//発注表をを参照し、発注表のデータを含め発注履歴に追加する。
 	//発注履歴に残すことが確認出来たら発注表のデータを消す？
-	public void addOrderHistory() {
+	public void addOrderHistory() throws Exception {
 		Connection con = null;
-		PreparedStatement ps = null;
-		String sql = null;
+
+		OrderTableDBAccess ot = new OrderTableDBAccess();
+		String[][] orderlist = CreateTableData.orderTableToArray(ot.getOrderTable());
+		int listSize = orderlist.length;
 		
 		//OrderCodeを生成する
-		String orderCode;		
+		String[] orderCodes = new String[listSize];//発注コード
 		//発注日を今日の日付に設定
 		LocalDate orderDate = LocalDate.now();//発注日
 		
+		String[] pCode = new String[listSize];//商品コード
+		String[] pName = new String[listSize];//商品名
+		String[] quantity = new String[listSize];//個数
+		String DeliveryDate = null;//納品予定
+		String Status = "確定";
+		String FoodLimitCode = null;
+		
+		for(int i = 0; i < listSize; i++) {
+			orderCodes[i] = createOrderCode(i);
+			pCode[i] = orderlist[i][0];
+			pName[i] = orderlist[i][1];
+			quantity[i] = orderlist[i][2];
+		}
+		for(int i = 0; i<listSize; i++) {
+			
+			String sql = "INSERT INTO(OrderCode,OrderDate,OrderQuantity,DeliveryDate,Status,FoodLimitCode) "
+					+ "VALUE(" 
+					+ orderCodes[i] +"," + orderDate+ "," + quantity[i] + "," + DeliveryDate +"," + Status + "," +FoodLimitCode +")";
+			try {
+				DBAccess db = new DBAccess();
+				PreparedStatement ps = null;
+				con = db.createConnection();
+				ps = con.prepareStatement(sql);
+				ps.executeQuery();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+	}
+	
+	//発注コードを作成
+	public String createOrderCode(int i) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		DBAccess db = new DBAccess();
+		ResultSet rs = null;
+		int count = 1;
+		String sql = "SELECT COUNT(*) FROM orders";
+		try {
+			con = db.createConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				count = rs.getInt("COUNT(*)");
+			}
+			count = count + i ;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if(rs != null) {
+					rs.close();
+				}
+				db.closeConnection(con);
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return Integer.toString(count);
 	}
 }
