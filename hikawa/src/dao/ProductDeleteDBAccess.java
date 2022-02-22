0c8bb5b08b7142d7a53fa48fa489c370a4529034
20jy0111 <92713057+20jy0111@users.noreply.github.com>
2022-02-22 14:46:40 +0900

    商品削除追加

---
+++ hikawa/src/dao/ProductDeleteDBAccess.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ProductDeleteDBAccess {
+	String pCode = null;
+	
+	public ProductDeleteDBAccess(String pCode){
+		//引数(商品コード)
+		this.pCode = pCode;
+	}
+	
+	//商品を削除する
+		public void ProductDelete() throws Exception {
+			Connection con = null;
+			DBAccess db = new DBAccess();
+			PreparedStatement ps = null;
+			
+			try {
+				con = db.createConnection();
+				String sql = "DELETE FROM product WHERE ProductCode = '" + pCode + "'";
+				
+				ps = con.prepareStatement(sql);
+				ps.executeUpdate();
+				con.commit();
+			} catch (Exception e) {
+				con.rollback();
+				e.printStackTrace();
+			}finally {
+				if(ps!= null) {
+					try {
+						ps.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					db.closeConnection(con);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+}
