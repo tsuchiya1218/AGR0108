86e31890ad8082e09a71c86387160f3967d63adf
20jy0134 <92972134+20jy0134@users.noreply.github.com>
2022-03-02 01:21:25 +0900

    売上と合うように調整しました

--- hikawa/src/model/Sales.java
+++ hikawa/src/model/Sales.java
@@ -2,60 +2,27 @@ package model;
 
 public class Sales {
 
-	private int SalesCode; //売上コード
-	private String Week; //週
-	private int wTotal; //週合計
-	private int mTotal; //月合計
-	private int yTotal; //年合計
+	private String Day; //週
+	private int Amount; //週合計
 
-	public Sales() {
+	public Sales(String Day, int Amount) {
+		this.Day = Day;
+		this.Amount = Amount;
 	}
 
-	public Sales(int SalesCode, int wTotal, int mTotal, int yTotal, String Week, int Day, int Sale) {
-		this.SalesCode = SalesCode;
-		this.wTotal = wTotal;
-		this.mTotal = mTotal;
-		this.yTotal = yTotal;
-		this.Week = Week;
+	public void setWeek(String Day) {
+		this.Day = Day;
 	}
 
-	public void setSalesCode(int SalesCode) {
-		this.SalesCode = SalesCode;
+	public String getDay() {
+		return Day;
 	}
 
-	public int SalesCode() {
-		return SalesCode;
+	public void setAmount(int Amount) {
+		this.Amount = Amount;
 	}
 
-	public void setwTotal(int wTotal) {
-		this.wTotal = wTotal;
-	}
-
-	public int getwTotal() {
-		return wTotal;
-	}
-
-	public void setmTotal(int mTotal) {
-		this.mTotal = mTotal;
-	}
-
-	public int getmTotal() {
-		return mTotal;
-	}
-
-	public void setyTotal(int yTotal) {
-		this.yTotal = yTotal;
-	}
-
-	public int getyTotal() {
-		return yTotal;
-	}
-
-	public void setWeek(String Week) {
-		this.Week = Week;
-	}
-
-	public String getWeek() {
-		return Week;
+	public int getAMount() {
+		return Amount;
 	}
 }
