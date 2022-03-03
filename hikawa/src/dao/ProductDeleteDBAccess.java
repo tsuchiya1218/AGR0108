70fecc9462f1c5a6e0857c6811b1c5baf1fa13c1
kame7 <kame7@kame>
2022-03-03 10:15:21 +0900

    DB変更に伴い変更しました

--- hikawa/src/dao/ProductDeleteDBAccess.java
+++ hikawa/src/dao/ProductDeleteDBAccess.java
@@ -5,22 +5,22 @@ import java.sql.PreparedStatement;
 
 public class ProductDeleteDBAccess {
 	String pCode = null;
-	
+
 	public ProductDeleteDBAccess(String pCode){
 		//引数(商品コード)
 		this.pCode = pCode;
 	}
-	
+
 	//商品を削除する
 		public void ProductDelete() throws Exception {
 			Connection con = null;
 			DBAccess db = new DBAccess();
 			PreparedStatement ps = null;
-			
+
 			try {
 				con = db.createConnection();
-				String sql = "DELETE FROM product WHERE ProductCode = '" + pCode + "'";
-				
+				String sql = "UPDATE product SET Invailed = 1 WHERE ProductCode =  '" + pCode + "'";
+
 				ps = con.prepareStatement(sql);
 				ps.executeUpdate();
 				con.commit();
