dd3d42d18437d326aea4c56c88b94abead9c06f6
20jy0134 <92972134+20jy0134@users.noreply.github.com>
2022-03-08 13:10:40 +0900

    同じ日付を入力したとき金額を更新

--- hikawa/src/dao/AddSalesAmountDBAccess.java
+++ hikawa/src/dao/AddSalesAmountDBAccess.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AddSalesAmountDBAccess {
 	public String date;
@@ -23,38 +25,115 @@ public class AddSalesAmountDBAccess {
 
 	//売り上げを入力し売上表に追加する
 	public void addSalesAmount() throws Exception {
-		Connection con = null;
-		DBAccess db = new DBAccess();
-		PreparedStatement ps = null;
+		int cnt = 0;
+		Connection con1 = null;
+		Connection con2 = null;
+		DBAccess db1 = new DBAccess();
+		DBAccess db2 = new DBAccess();
+		PreparedStatement ps1 = null;
+		PreparedStatement ps2 = null;
+		ResultSet rs = null;
+
 		try {
-			con = db.createConnection();
-			//INSERT INTO テーブル名 (列名1, 列名2,...) VALUES (値1, 値2,...);
-			String sql = "INSERT INTO sales (Date,Amount) "
-					+ "VALUES(?,?)";
+			con1 = db1.createConnection();
+			
+			String sql = "SELECT COUNT(*) as cnt FROM sales WHERE Date = '" + date + "'";
 
-			ps = con.prepareStatement(sql);
+			ps1 = con1.prepareStatement(sql);
 
-			ps.setString(1, date);
-			ps.setInt(2, amount);
+			rs = ps1.executeQuery();
 
-			ps.executeUpdate();
-			con.commit();
+			while (rs.next()) {
+				cnt = rs.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("データベース接続のエラーです。");
 		} catch (Exception e) {
-			con.rollback();
 			e.printStackTrace();
 		} finally {
-			if (ps != null) {
+			if (rs != null) {
 				try {
-					ps.close();
+					rs.close();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
+			if (ps1 != null) {
+				try {
+					ps1.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			db1.closeConnection(con1);
+		}
+
+		if (cnt == 0) {
 			try {
-				db.closeConnection(con);
+				con2 = db2.createConnection();
+				//INSERT INTO テーブル名 (列名1, 列名2,...) VALUES (値1, 値2,...);
+				String sql = "INSERT INTO sales (Date,Amount) "
+						+ "VALUES(?,?)";
+
+				ps2 = con2.prepareStatement(sql);
+
+				ps2.setString(1, date);
+				ps2.setInt(2, amount);
+
+				ps2.executeUpdate();
+				con2.commit();
 			} catch (Exception e) {
+				con2.rollback();
 				e.printStackTrace();
+			} finally {
+				if (ps2 != null) {
+					try {
+						ps2.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					db2.closeConnection(con2);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			try {
+				con2 = db2.createConnection();
+				//INSERT INTO テーブル名 (列名1, 列名2,...) VALUES (値1, 値2,...);
+				String sql = "UPDATE sales "
+						+ "SET Amount = ? "
+						+ "WHERE Date = ?";
+
+				ps2 = con2.prepareStatement(sql);
+
+				ps2.setString(2, date);
+				ps2.setInt(1, amount);
+
+				ps2.executeUpdate();
+				con2.commit();
+			} catch (Exception e) {
+				con2.rollback();
+				e.printStackTrace();
+			} finally {
+				if (ps2 != null) {
+					try {
+						ps2.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					db2.closeConnection(con2);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
+		
+
 	}
 }
